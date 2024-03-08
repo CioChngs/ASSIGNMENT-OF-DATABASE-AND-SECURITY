1effc11f04c5bc015356d219d84af5531a8a26b0
CioChngs <csciodev0@gmail.com>
2024-03-08 19:16:28 +0800

    adding changes.

--- test/database.php
+++ test/database.php
@@ -4,11 +4,11 @@ $username = "root";
 $password = "";
 $database = "coffee_shop";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$conn->close();
 ?>
